f4bd903512ec1a8f5e6244cbcada5e444e9a9690
Federico Lattanzi <34736331+federico-lattanzi@users.noreply.github.com>
2019-08-05 17:43:10 +0200

    Update Test.java

--- Errori4/src/errori4/Test.java
+++ Errori4/src/errori4/Test.java
@@ -5,19 +5,19 @@ public class Test implements Runnable{//La classe doveva essere implementata e n
 	private Thread thread;
 	
 	public Test() {
-		this.thread=null;
+		this.thread=new Thread(this);//Istanziare il thread inserendo come parametro il runnable ovvero la classe stessa.
 	}
 
 	@Override//Mancava l'ovverride
 	public void run() {
 		int i = 0;
 		for(i=0;i<10;i++) {
-			System.out.println("i="+i);
+			System.out.println(i=+i);//rimuovere i doppi apici, poiché deve restituire la somma.
 		}
 	}
 	
 	public static void main(String[] args) {
 		Test t = new Test();
-		t.run();//Non era start ma run
+		t.start();
 	}
 }
